08a3ae72cc0334cb0aa3c2cf966df3010d10d9d8
querenneves <querensantos1@hotmail.com>
2023-04-12 11:02:01 -0300

    Projeto Hello world

---
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@GetMapping
+	public String hello() {
+		return "<b>Hello Turma 62!</b>";
+	}
+	
+	@GetMapping("/nome")
+	public String meuNome() {
+		return "<b>Queren chorando! <b> <b>  teste </b>";
+	}
+}
